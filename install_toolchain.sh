630b9ffa889f4234cf6d03deab0560a6d41a49f6
nmcicek <cicek.nihatmert@gmail.com>
2022-06-22 18:00:37 +0000

    fixed sh scripts

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 base_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $base_dir/riscv-gnu-toolchain
-./configure --prefix=$RISCV
-make -j10
-make linux -j10
+cd $base_dir/riscv-gnu-toolchain && ./configure --prefix=$RISCV 
+cd $base_dir/riscv-gnu-toolchain && make -j10
+cd $base_dir/riscv-gnu-toolchain && make linux -j10
