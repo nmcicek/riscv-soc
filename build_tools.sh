4f181e49be52cbbc70c9fbef9efaa99fc4e59290
nmcicek <cicek.nihatmert@gmail.com>
2022-06-22 17:54:51 +0000

    fixed sh scripts

--- build_tools.sh
+++ build_tools.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 base_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $base_dir/riscv-tools && git submodule update --init --recursive && ./build.sh
+cd $base_dir/riscv-tools && git submodule update --init --recursive . && ./build.sh
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 base_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $base_dir/riscv-tools && git submodule update --init --recursive
-cd $base_dir/riscv-tools/riscv-tests && git pull origin master && git submodule update --init --recursive
-cd $base_dir/riscv-tools/riscv-pk && git pull origin master && git submodule update --init --recursive
-cd $base_dir/rocket-chip && git submodule update --init --recursive
+cd $base_dir/riscv-tools && git submodule update --init --recursive .
+cd $base_dir/riscv-tools/riscv-tests && git pull origin master && git submodule update --init --recursive .
+cd $base_dir/riscv-tools/riscv-pk && git pull origin master && git submodule update --init --recursive .
+cd $base_dir/rocket-chip && git submodule update --init --recursive .
