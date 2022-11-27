d3b0ae9e3f752c5673a8d2a19360c58965979af9
nmcicek <cicek.nihatmert@gmail.com>
2022-11-27 20:16:23 +0000

    updated installation scripts

--- install_tools/build_tools.sh
+++ install_tools/build_tools.sh
@@ -3,4 +3,4 @@ base_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $base_dir/riscv-tools && git submodule update --init --recursive .
 cd $base_dir/riscv-tools/riscv-tests && git pull origin master && git submodule update --init --recursive .
 cd $base_dir/riscv-tools/riscv-pk && git pull origin master && git submodule update --init --recursive .
-cd $base_dir/riscv-tools && ./build.sh
+cd $base_dir/riscv-tools && sed -i 's/nostartfiles/& -fcommon/' riscv-tests/isa/Makefile && ./build.sh
